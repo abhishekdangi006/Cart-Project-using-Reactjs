783cb89f37fe6dae1bba7a335cf77cbf96fce271
Abhishek Dangi <abhishekdangi006@gmail.com>
2023-03-17 09:57:49 +0530

    IncreaseQuantity and decreaseQuantity function has been implemented

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,75 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor () {
+        super();
+        this.state = {
+            price: 8000,
+            title: 'Telephone',
+            qty: 1,
+            img: ''
+        }
+    }
+
+    increaseQuantity=()=>{
+        // setState form 1
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // })
+
+        //setState form 2 
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty + 1
+            }
+        });
+    }
+
+    decreaseQuantity=()=>{
+        const {qty} = this.state;
+        if(qty === 0){
+            return;
+        }
+
+        this.setState((prevState) => {
+            // if(prevState.qty == 1){
+            //     return {
+            //         qty: prevState.qty
+            //     }
+            // }else{
+                return {
+                    qty: prevState.qty - 1
+                }
+            // }
+        });
+    }
+
     render() {
+        const {title, price, qty} = this.state;
         return(
             <div className='cart-item'>
                 <div className='left-block'>
-                    <img style={styles.image}/>
+                    <img alt='' style={styles.image}/>
                 </div>
                 <div className='right-block'>
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs 999</div>
-                    <div style={{color:'#777'}}>Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>Rs {price}</div>
+                    <div style={{color:'#777'}}>Qty: {qty}</div>
                     <div className='cart-item-actions'>
-                        {/* Buttons*/}
+                        <img alt='increase' 
+                        className='action-icons' 
+                        src='https://cdn-icons-png.flaticon.com/512/992/992651.png' 
+                        onClick={this.increaseQuantity}
+                        />
+                        <img alt='decrease' 
+                        className='action-icons' 
+                        src='https://cdn-icons-png.flaticon.com/512/992/992683.png'
+                        onClick={this.decreaseQuantity} 
+                        />
+                        <img alt='delete' 
+                        className='action-icons' 
+                        src='https://cdn-icons-png.flaticon.com/512/484/484662.png' 
+                        />
                     </div>
                 </div>
             </div>
